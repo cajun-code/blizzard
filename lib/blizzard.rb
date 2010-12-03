5d34aff7cfd0f72fba36846df1f5dff97e532958
Allan Davis <javaalley@gmail.com>
2010-12-03 07:26:11 -0500

    broke flake into own class

--- lib/blizzard.rb
+++ lib/blizzard.rb
@@ -1,23 +1,48 @@
 require 'gosu'
 
 module Blizzard
+  FILE_PATH = File.expand_path(File.dirname(__FILE__))
+  class Flake
+    def initialize(window)
+      @window = window
+      @x = rand(@window.witdh)
+      @y = -1
+      file_name = File.join(FILE_PATH, "media", "Blizzard.png")
+      @image = Gosu::Image.new(self, file_name , true)
+    end
+    
+    def update
+    end
+    
+    def draw
+      @image.draw(@x ,@y, 0)
+    end
+  end
   
   class BlizzardApp < Gosu::Window
-    FILE_PATH = File.expand_path(File.dirname(__FILE__))
+    
     SCREEN_WIDTH = 640
     SCREEN_HEIGHT = 480
     def initialize()
       super(SCREEN_WIDTH, SCREEN_HEIGHT, false)
       self.caption = "Blizzard attack"
-      file_name = File.join(FILE_PATH, "media", "Blizzard.png")
-      puts file_name
-      @flake = Gosu::Image.new(self, file_name , true)
+      @flakes = []
+      @speed = 200
+      @step = 25
+      @counter = 0
+      @reset = 100000
     end
     def update
+      
     end
     
     def draw
-      @flake.draw((SCREEN_WIDTH - @flake.width)/2 ,(SCREEN_HEIGHT - @flake.height)/2, 0)
+      @flakes.each do |flake|
+        flake.draw
+      end
+    end
+    def reset
+      @counter = 0
     end
   end
   
